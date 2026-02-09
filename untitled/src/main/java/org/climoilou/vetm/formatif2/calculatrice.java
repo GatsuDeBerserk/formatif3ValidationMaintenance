d6edf58af23314f359ec5efa9f401f334f596ccd
diesel <etienne.lavoie007@gmail.com>
2026-02-09 09:00:29 -0500

    debut projet

---
+++ untitled/src/main/java/org/climoilou/vetm/formatif2/calculatrice.java
@@ -0,0 +1,15 @@
+package org.climoilou.vetm.formatif2;
+
+public class calculatrice {
+
+    public calculatrice() {
+
+    }
+
+    public int add(String nombre) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        new calculatrice();
+    }
+}
